ffa885ef9113b23ba2595b7cd8cbdf33021510aa
Vincent KERDRAON <vincent.kerdraon@gmail.com>
2025-06-06 15:14:46 -0600

    version 1.6

--- farmer-school-bevy/src/model/config.rs
+++ farmer-school-bevy/src/model/config.rs
@@ -2,7 +2,7 @@ use std::path::PathBuf;
 
 use bevy::prelude::*;
 
-pub const VERSION: &str = "v1.5";
+pub const VERSION: &str = "v1.6";
 pub const REPO: &str = "https://gitlab.com/eclypsaine/farmer-school";
 
 #[derive(Debug, Clone, Resource)]
